185a1f5a787443f26322f453d19c95972dd85d18
Martin Hencl <martin.hencl@email.cz>
2018-11-04 21:33:07 +0100

    První commit

---
+++ code/html/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="cs">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Výpůjčky a pronájem vozidel" />
+    <meta name="keywords" content="Vozidla, auta, registrace, výpůjčky, půjčení, pronájem, osobní, dodávky" />
+    <meta name="author" content="all: Martin Hencl; st5250@student.upce.cz" />
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot" content="index, follow, snippet, archive" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" /> <!-- optimalizace pro iPhone X -->
+
+    <title>Výpůjčky a pronájem vozidel</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="../image/Graphicloads-Colorful-Long-Shadow-Car.ico" /> <!-- http://www.iconarchive.com/show/colorful-long-shadow-icons-by-graphicloads/Car-icon.html -->
+
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1025px)" href="../css/styly_display.css" />
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1024px)" href="../css/styly_mobile.css" />
+
+
+</head>
+
+<body>
+
+<main>
+
+    <header>
+        <div class="container" >
+            <img src="../image/mercedes-benz-c-class-vehicle-model-banner.jpg" alt="Banner auto"  >    <!--  https://www.mercedes-benz-newmarket.ca/about-us/mercedes-benz-c-class-vehicle-model-banner/ -->
+        </div>
+    </header>
+
+    <nav>
+    </nav>
+
+    <!-- Boční sloupec
+    <aside>
+    </aside>
+    -->
+
+    <section>
+
+        <article>
+        </article>
+
+        <article>
+        </article>
+
+    </section>
+
+    <footer>
+    </footer>
+
+</main>
+
+</body>
+
+</html>
